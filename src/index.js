19d27e1d204b62c0f68b73886048fb598df44c20
BhargavMantha <manthabhargav@gmail.com>
2021-09-12 21:22:24 +0530

    fixed the 1. for invalied/ forbidden property name error

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-function buttonClickHandler() {
-  return "Click Me";
-}
+
 /**
  * Create a react component
  */
 const App = () => {
+  const buttonText = "Click Me";
   return (
     <div>
-      <label className="label" for="name">
+      <label className="label" htmlFor="name">
         Enter Name:
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        {buttonClickHandler()}
+        {buttonText}
       </button>
     </div>
   );
